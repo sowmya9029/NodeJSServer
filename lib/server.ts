0c9c231f285a7bdd294c0ee1ebba783b42a0185a
sowmya <sowmya9029@gmail.com>
2019-06-05 18:55:08 -0700

    changed according to professors steps

--- lib/server.ts
+++ lib/server.ts
@@ -2,12 +2,12 @@ import app from "./app";
 import {DataAccess} from '../DataAccess';
 
 const PORT = 8080;
-const port= process.env.PORT;
+
 console.log("before connect");
 DataAccess.connect();
-
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080);
+/*app.listen(PORT, () => {
     
     console.log('Express server listening on port ' + PORT);
     
-})
+})*/

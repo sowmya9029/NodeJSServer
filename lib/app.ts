d6a6d0911ece6322cdd35207abb958792e0654b8
sowmya <sowmya9029@gmail.com>
2019-06-06 00:07:45 -0700

    changing the app and routes file

--- lib/app.ts
+++ lib/app.ts
@@ -14,12 +14,11 @@ class App {
         this.app.use(cors());
         this.config();      
         this.routePrv.routes(this.app);    
-       
+        this.app.use('/', express.static(__dirname + '/angularDist'));
     }
 
     private config(): void{
         // support application/json type post data
-        this.app.use('/', express.static(__dirname+'/angularDist'));
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));

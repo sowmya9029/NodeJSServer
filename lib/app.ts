cafbecf32131e4e148eb0ac9f797299177fe57bb
sowmya <sowmya9029@gmail.com>
2019-06-06 23:31:04 -0700

    test2

--- lib/app.ts
+++ lib/app.ts
@@ -14,7 +14,10 @@ class App {
         this.app.use(cors());
         this.config();      
         this.routePrv.routes(this.app);    
-       
+        this.app.get('/', function(req, res) {
+            res.sendfile(__dirname+'/angularDist/index.html')
+        
+        });
     }
 
     private config(): void{
@@ -23,7 +26,9 @@ class App {
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
-        this. app.use('/', express.static(__dirname+'/angularDist'));
+        //this. app.use('/', express.static(__dirname+'/angularDist'));
+
+       
     }
     
 
